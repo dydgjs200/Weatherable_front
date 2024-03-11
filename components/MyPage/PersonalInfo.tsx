12afa1ce6129cb2cf92cf164159abb135794c4ac
kimjaehyeon0527 <kimdh66665@naver.com>
2024-03-11 13:39:43 +0900

    03.11 mypage 컴포넌트 변경

---
+++ components/MyPage/PersonalInfo.tsx
@@ -0,0 +1,5 @@
+function PersonalInfo() {
+  return <>개인정보 입니다.</>;
+}
+
+export default PersonalInfo;
---
+++ components/MyPage/Statistics.tsx
@@ -0,0 +1,5 @@
+function Statistics() {
+  return <>통계 부분 입니다.</>;
+}
+
+export default Statistics;
