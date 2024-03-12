d493ac8ba4a70b3b3313dc25fc78c86387e61ba3
kimjaehyeon0527 <kimdh66665@naver.com>
2024-03-13 01:18:28 +0900

    03.12 치수 grid 작업중

--- components/MyPage/Dimension.tsx
+++ components/MyPage/Dimension.tsx
@@ -1,5 +1,16 @@
+import styles from '../../styles/MyPage/Dimension.module.scss';
+
 function Dimension() {
-  return <>치수 입니다.</>;
+  return (
+    <>
+      <div className={styles.gridContainer}>
+        <div className={styles.gridItem}>Item 1</div>
+        <div className={styles.gridItem}>Item 2</div>
+        <div className={styles.gridItem}>Item 3</div>
+        <div className={styles.gridItem}>Item 4</div>
+      </div>
+    </>
+  );
 }
 
 export default Dimension;
