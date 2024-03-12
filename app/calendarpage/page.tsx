96326a2e79bfbd16e37cb164315fdf08ed88c1d5
wjdgns4070 <ljj888800@naver.com>
2024-03-12 13:56:39 +0900

    2403124

--- app/calendarpage/page.tsx
+++ app/calendarpage/page.tsx
@@ -1,11 +1,13 @@
 'use client'; // nextjs에서 useState사용시 작성필
 import React from 'react';
 import Calendar from '../../components/calendarpage/calendarpage';
-
+import Styles from '../../styles/calendar/calendar.module.scss';
 const CalendarPage: React.FC = () => {
   return (
-    <div>
-      <h1>Calendar Page</h1>
+    <div className={Styles.all}>
+      <div className={Styles.h1}>
+        <h1>Calendar Page</h1>
+      </div>
       <Calendar />
     </div>
   );
--- components/calendarpage/calendarpage.tsx
+++ components/calendarpage/calendarpage.tsx
@@ -25,22 +25,34 @@ const Calendar = () => {
     }
   };
 
+  const today = (day: number, month: number) => {
+    return new Date(day, month, 0).getDate();
+  };
   const daysInMonth = (year: number, month: number) => {
     return new Date(year, month, 0).getDate();
   };
 
   const renderCalendar = () => {
     const totalDays = daysInMonth(year, month);
+    const today = new Date(); // 오늘 날짜를 가져옵니다.
     const firstDayOfWeek = new Date(year, month - 1, 1).getDay();
-
     const calendar = [];
     let row = [];
+
     for (let i = 0; i < firstDayOfWeek; i++) {
       row.push(<td key={`empty-${i}`} className={styles['empty-cell']} />);
     }
 
     for (let day = 1; day <= totalDays; day++) {
-      row.push(<td key={day}>{day}</td>);
+      const isToday =
+        today.getFullYear() === year &&
+        today.getMonth() + 1 === month &&
+        today.getDate() === day; // 현재 날짜인지 확인합니다.
+      row.push(
+        <td key={day} className={isToday ? styles['today'] : undefined}>
+          {day}
+        </td> // 오늘 날짜인 경우 'today' 클래스를 추가합니다.
+      );
       if ((firstDayOfWeek + day) % 7 === 0) {
         calendar.push(<tr key={day}>{row}</tr>);
         row = [];
@@ -57,11 +69,13 @@ const Calendar = () => {
   return (
     <div className={styles['calendar-container']}>
       <div className={styles['calendar-header']}>
+        <div className={styles['calendar-buttons']}>
+          <button onClick={prevMonth}>이전 달</button>
+        </div>
         <div className={styles['calendar-title']}>
           {year}년 {month}월
         </div>
         <div className={styles['calendar-buttons']}>
-          <button onClick={prevMonth}>이전 달</button>
           <button onClick={nextMonth}>다음 달</button>
         </div>
       </div>
