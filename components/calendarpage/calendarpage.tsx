dec7036a8aaf57e51965303334ad572cd69d0c9c
wjdgns4070 <ljj888800@naver.com>
2024-03-13 23:34:41 +0900

    240313

--- components/calendarpage/calendarpage.tsx
+++ components/calendarpage/calendarpage.tsx
@@ -1,11 +1,16 @@
 'use client'; // nextjs에서 useState사용시 작성필
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../../styles/calendar/calendar.module.scss';
 
 const Calendar = () => {
   const [year, setYear] = useState<number>(new Date().getFullYear());
   const [month, setMonth] = useState<number>(new Date().getMonth() + 1);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때 오늘 날짜를 선택한 날짜로 설정합니다.
+    setSelectedDate(getFormattedDate(new Date()));
+  }, []);
 
   const prevMonth = () => {
     if (month === 1) {
@@ -25,16 +30,20 @@ const Calendar = () => {
     }
   };
 
-  const today = (day: number, month: number) => {
-    return new Date(day, month, 0).getDate();
+  const handleClick = (day: number) => {
+    setSelectedDate(`${year}년 ${month}월 ${day}일`);
   };
-  const daysInMonth = (year: number, month: number) => {
-    return new Date(year, month, 0).getDate();
+
+  const getFormattedDate = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}년 ${month}월 ${day}일`;
   };
 
   const renderCalendar = () => {
-    const totalDays = daysInMonth(year, month);
-    const today = new Date(); // 오늘 날짜를 가져옵니다.
+    const totalDays = new Date(year, month, 0).getDate();
+    const today = new Date();
     const firstDayOfWeek = new Date(year, month - 1, 1).getDay();
     const calendar = [];
     let row = [];
@@ -47,11 +56,20 @@ const Calendar = () => {
       const isToday =
         today.getFullYear() === year &&
         today.getMonth() + 1 === month &&
-        today.getDate() === day; // 현재 날짜인지 확인합니다.
+        today.getDate() === day;
+      const isSelected = selectedDate === `${year}년 ${month}월 ${day}일`;
       row.push(
-        <td key={day} className={isToday ? styles['today'] : undefined}>
+        <td
+          key={day}
+          className={`${
+            isToday && !selectedDate ? styles['today'] : undefined
+          } ${isSelected ? styles['selectedDate'] : undefined} ${
+            styles['calendar-day']
+          }`}
+          onClick={() => handleClick(day)}
+        >
           {day}
-        </td> // 오늘 날짜인 경우 'today' 클래스를 추가합니다.
+        </td>
       );
       if ((firstDayOfWeek + day) % 7 === 0) {
         calendar.push(<tr key={day}>{row}</tr>);
@@ -64,6 +82,22 @@ const Calendar = () => {
     }
 
     return calendar;
+  };
+
+  const renderUploadButton = () => {
+    const today = new Date();
+    const selectedDay = selectedDate
+      ? parseInt(selectedDate.split(' ')[2].replace('일', ''), 10)
+      : null;
+
+    if (selectedDay && Math.abs(selectedDay - today.getDate()) <= 1) {
+      return <button onClick={upload}>등록하기</button>;
+    }
+    return null;
+  };
+
+  const upload = () => {
+    console.log('등록하기');
   };
 
   return (
@@ -94,6 +128,10 @@ const Calendar = () => {
           </thead>
           <tbody>{renderCalendar()}</tbody>
         </table>
+      </div>
+      <div className={styles['selected-date-box']}>
+        <p>선택한 날짜: {selectedDate || getFormattedDate(new Date())}</p>
+        {renderUploadButton()}
       </div>
     </div>
   );
