f8a080fbf7f94caa4355d6e1024c2319d21d8bd6
wjdgns4070 <ljj888800@naver.com>
2024-03-17 02:46:14 +0900

    240317

--- components/uploadcloset/uploadClosetInfoBox.tsx
+++ components/uploadcloset/uploadClosetInfoBox.tsx
@@ -1,20 +1,31 @@
 'use client';
 
+import React from 'react';
 import styles from '../../styles/closet/closet.module.scss';
-import Link from 'next/link';
 
-export default function ClothesInfoBox() {
+interface ClothesInfoBoxProps {
+  onImageClick: (imageUrl: string) => void;
+}
+
+const ClothesInfoBox: React.FC<ClothesInfoBoxProps> = ({ onImageClick }) => {
   return (
-    <div className={styles.infoSmallBox}>
-      <Link href="/clothes/1">
-        <div>
-          <span>제품명</span>
-        </div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png"
-          alt=""
-        />
-      </Link>
+    <div
+      className={styles.infoSmallBox}
+      onClick={() =>
+        onImageClick(
+          'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png'
+        )
+      }
+    >
+      <div>
+        <span>제품명</span>
+      </div>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png"
+        alt=""
+      />
     </div>
   );
-}
+};
+
+export default ClothesInfoBox;
