54878f1e633ae32583505fe59142573ff9b516eb
kimjaehyeon0527 <kimdh66665@naver.com>
2024-03-19 13:21:26 +0900

    03.19 회원가입 유효성 검사 완료

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -77,7 +77,6 @@ const Login: React.FC = () => {
       alert(
         '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.'
       );
-      setUserData({ userid: '', password: '' }); // 입력값 초기화
     }
   };
 
