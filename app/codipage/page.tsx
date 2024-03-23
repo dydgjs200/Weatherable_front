01d14f66b5eafababd0e511b0cb0c4e1fb05ef8f
wjdgns4070 <ljj888800@naver.com>
2024-03-23 11:53:18 +0900

    2403233

--- app/codipage/page.tsx
+++ app/codipage/page.tsx
@@ -1,8 +1,20 @@
+'use client';
+import React, { useEffect } from 'react';
 import Styles from '../../styles/calendar/calendar.module.scss';
 import Codicomponent from '../../components/codipage/codicomponent';
 import SelectedDateDisplay from '../../components/codipage/date';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../Store/Store';
 
-const CodiPage: React.FC<{ selectedDate: string }> = ({ selectedDate }) => {
+const CodiPage: React.FC<{ selectedDate: string; userId: string }> = () => {
+  const selectedDate = useSelector(
+    (state: RootState) => state.calendar.selectedDate
+  );
+
+  useEffect(() => {
+    // 여기에서 필요한 작업 수행
+  }, []);
+
   return (
     <div>
       <h1>코디 페이지</h1>
