fd76883ba243f53ac36549903240ff6c66acd083
nwejin <147528674+nwejin@users.noreply.github.com>
2024-03-26 09:59:58 +0900

    ai 추천 페이지

--- app/AIrecommend/page.tsx
+++ app/AIrecommend/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { getUserClothes, aiRecommendGet } from '../../service/closetApiService';
 import { useSelector } from 'react-redux';
-
+import { useRouter } from 'next/navigation';
 interface clothes {
   brand: string;
   color: string | null;
@@ -26,16 +26,33 @@ interface clothes {
 }
 
 export default function AiRecommend() {
+  const router = useRouter();
+
+  // 유저 옷 정보 저장
   const [isUserData, setIsUserData] = useState<clothes[]>([]);
+
+  // 날씨 데이터 받아오기
   const weatherData = useSelector((state: any) => ({
     temp: state.aiRecommend.weather.temp,
     weather: state.aiRecommend.weather.weather,
   }));
-  const [isData, setIsData] = useState('');
+  console.log(weatherData);
+
+  // Ai 전달용 데이터
+  const [isAiData, setIsAiData] = useState('');
 
   useEffect(() => {
+    // 날씨 정보가 없으면 뒤로가기 (너무 빨리 접속 시 날씨 데이터를 못불러옴)
+    if (weatherData.temp == 0) {
+      alert('온도 정보가 없습니다! 메인 화면에서 온도 정보를 불러와주세요');
+      router.back();
+    } else if (weatherData.weather == '') {
+      alert('날씨 정보가 없습니다! 메인 화면에서 날씨 정보를 불러와주세요');
+      router.back();
+    }
     const fetchData = async () => {
       try {
+        // 유저 옷 정보 불러오기
         const userClothData = await getUserClothes();
         setIsUserData(userClothData);
       } catch (error) {
@@ -45,16 +62,15 @@ export default function AiRecommend() {
     fetchData();
   }, []);
 
-  console.log(weatherData);
-
-  if (isUserData.length === 0) {
-    return <div>Loading...</div>;
-  }
+  // if (isUserData.length === 0) {
+  //   return <div>Loading...</div>;
+  // }
 
   useEffect(() => {
     const transformedData: any = {
       weather: [weatherData.temp, weatherData.weather],
     };
+
     const groupByData = (isUserData: clothes[]) => {
       return isUserData.reduce((groups: any, item: clothes) => {
         let category: string;
@@ -78,73 +94,27 @@ export default function AiRecommend() {
         return groups;
       }, {});
     };
-
     const newGroup = groupByData(isUserData);
 
     for (const category in newGroup) {
       transformedData[category] = newGroup[category];
     }
 
-    setIsData(transformedData);
-  }, [isUserData, weatherData]);
+    setIsAiData(transformedData);
+  }, [isUserData]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const userClothData = await aiRecommendGet(isData);
+        console.log(isAiData);
+        const userClothData = await aiRecommendGet(isAiData);
+        console.log(userClothData);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
-  }, [isData]);
-
-  const groupByData = (isUserData: clothes[]) => {
-    return isUserData.reduce((groups: any, item: clothes) => {
-      let category: string;
-      switch (item.majorCategory) {
-        case 'Top':
-        case 'Outer':
-        case 'Shoes':
-        case 'Accessory':
-          category = item.majorCategory;
-          break;
-        case 'Pants':
-        case 'Skirt':
-        case 'Onepiece':
-          category = 'Bottom';
-      }
-
-      if (!groups[category]) {
-        groups[category] = [];
-      }
-      groups[category].push([item.productName, item.middleCategory]);
-      return groups;
-    }, {});
-  };
-
-  const newGroup = groupByData(isUserData);
-
-  const transformedData: any = {
-    weather: [weatherData.temp, weatherData.weather],
-  };
-
-  for (const category in newGroup) {
-    transformedData[category] = newGroup[category];
-  }
-
-  setIsData(transformedData);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const userClothData = await aiRecommendGet(isData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, [isData]);
+  }, [isAiData]);
 
   return (
     <div>
